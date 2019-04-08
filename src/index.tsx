3bbd1817bb726b8352c9c9f256fb491e70d48bfa
DeanDK <deanbozic12@gmail.com>
2019-04-08 20:17:57 +0200

    added useState hook

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import ReactDOM from "react-dom";
 
+/*
+@value must be string
+@setValue must return string
+*/
+
+type FormElem = React.FormEvent<HTMLFormElement>;
 export default function App(): JSX.Element {
-  return <h1>Welcome :)</h1>;
+  const [value, setValue] = useState<string>("");
+
+  const handleSubmit = (e: FormElem): void => {
+    e.preventDefault();
+    setValue("");
+  };
+
+  return (
+    <>
+      <h1>Todo List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+          required
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </>
+  );
 }
 
 const root = document.getElementById("app-root");
